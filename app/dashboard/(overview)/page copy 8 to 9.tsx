db1e39f4b8c4bde3072cbbc3b7114aa14ed92f40
#55263025 <hardilpg123@gmail.com>
2024-01-10 18:00:32 +0700

    first commit

--- app/dashboard/(overview)/page copy 8 to 9.tsx
+++ app/dashboard/(overview)/page copy 8 to 9.tsx
@@ -1,3 +1,4 @@
+/*
 import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
@@ -28,11 +29,12 @@ export default async function Page() {
         />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        {/* Remove tag RevenueChart for materi 9 */}
-        {/* <RevenueChart revenue={revenue}  /> */}
+        // Remove tag RevenueChart for materi 9 
+        // <RevenueChart revenue={revenue}  />
         
         <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
   );
 }
+*/
